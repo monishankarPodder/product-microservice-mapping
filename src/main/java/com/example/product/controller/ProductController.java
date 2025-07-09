cc0dc643643ca62bdd94f656d3c42131d43d209c
monishankarPodder <monishankar.podder@gmail.com>
2025-07-09 18:49:08 +0530

    Update ProductController.java

--- src/main/java/com/example/product/controller/ProductController.java
+++ src/main/java/com/example/product/controller/ProductController.java
@@ -8,6 +8,6 @@ public class ProductController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello Product Service!";
+        return "Hello Product Service1!";
     }
 }
