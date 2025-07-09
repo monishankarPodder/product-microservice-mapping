a115e5cb6d968ffb38b5336bd823753e4efa97a8
monishankarPodder <monishankar.podder@gmail.com>
2025-07-09 20:05:36 +0530

    Create OrderController.java

---
+++ src/main/java/com/example/product/controller/OrderController.java
@@ -0,0 +1,15 @@
+package com.example.product.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Order Service!";
+    }
+}
